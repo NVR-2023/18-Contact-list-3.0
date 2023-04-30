768e1d6ac965438fd20db0050c92986cef4813d9
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-04-30 19:36:31 +0000

    Pending changes exported from your codespace

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,44 +1,87 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			agendaSlug: "Agenda2023Q2",
+			contactList: [],
 		},
 		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
-			loadSomeData: () => {
-				/**
-					fetch().then().then(data => setStore({ "foo": data.bar }))
-				*/
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+			// Displays Alerts, available to every other method 
+			alertUser: (message, color) => {
+				const alertElement = document.createElement("div");
+				alertElement.classList.add("alertToUser");
+				alertElement.textContent = message;
+				alertElement.style.backgroundColor = color;
+				document.body.appendChild(alertElement);
+				setTimeout(() => {
+					alertElement.remove();
+				}, 2100);
+			},
 
-				//reset the global store
-				setStore({ demo: demo });
+			// Data management functions
+			createAgenda: async () => {
+				const initializationObject = {
+					"full_name": "Initialization",
+					"email": "none@none.com",
+					"agenda_slug": getStore().agendaSlug,
+					"address": "none",
+					"phone": "999999999"
+				}
+
+				try {
+					const response = await fetch("https://assets.breatheco.de/apis/fake/contact/", {
+						method: "POST",
+						headers: { "Content-type": "application/json" },
+						body: JSON.stringify(initializationObject)
+					});
+					if (response.ok) {
+						getActions().alertUser("agenda created", "green");
+						return null;
+					} else {
+						throw new Error("Failed to create agenda");
+					}
+				} catch (error) {
+					return error;
+				}
+			},
+
+			fetchAgenda: async () => {
+				try {
+					const response = await fetch("https://assets.breatheco.de/apis/fake/contact/" + getStore().agendaSlug);
+					const data = await response.json();
+					setStore({ contactList: data });
+					return null;
+				} catch (error) {
+					return error;
+				}
+			},
+
+			initializeAgenda: async () => {
+				try {
+					let response = await getActions().fetchAgenda();
+
+					if (response && response.length > 0) {
+						getActions().alertUser("agenda initialized", "green");
+						return null;
+					}
+
+					response = await getActions().createAgenda();
+					if (response) {
+						getActions().alertUser("agenda initialized", "green");
+						return null;
+					}
+
+					throw new Error("Failed to initialize agenda");
+				} catch (error) {
+					getActions().alertUser("failed to initialize agenda", "red");
+					return error;
+				}
 			}
-		}
+
+
+
+
+		},
 	};
 };
 
