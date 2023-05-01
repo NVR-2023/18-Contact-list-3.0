105fa7dacc7bdec9143e7eed7b7be9aabba1be86
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-05-01 16:26:45 +0000

    css refinements 01/04/2023

--- src/js/component/contactCard.js
+++ src/js/component/contactCard.js
@@ -1,23 +1,29 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { Context } from "../store/appContext"
+import { Context } from "../store/appContext";
 
-const ContactCard = ({ index, photo, full_name, address, phone, email }) => {
+const ContactCard = ({ index, photo: image, full_name, address, phone, email }) => {
     const { store, actions } = useContext(Context);
+    
+    let imageLabel="";
+    if (!image) { 
+        image = "https://dummyimage.com/200x200/28a745/fff.png&text=+"
+        imageLabel="add photo";
+    }
 
     return (
         <div className="contactCard-wrapper">
             <div className="card contactCard">
-                <div className="card-header">
-                    {photo ? <img src={photo} className="card-img-top circle" alt="contact photo" /> :
-                        <div className="card-icon-top">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
-                                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3Zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z" />
-                            </svg>
-                        </div>
-                    }
+                <div className="card-header cardHeader">
+                    <div className="iconContainer">
+                        <div className="editIcon"></div>
+                        <div className="deleteIcon"></div>
+                    </div>
+                    <div className="imageContainer">
+                        <img src={image} />
+                        {imageLabel && <div className="imageLabel">{imageLabel}</div>}
+                    </div>
                 </div>
-
                 <div className="card-body cardSection">
                     <div className="cardLabels">
                         <label>name:</label>
@@ -26,7 +32,7 @@ const ContactCard = ({ index, photo, full_name, address, phone, email }) => {
                         <label>address:</label>
                     </div>
                     <div className="cardInfo">
-                        <div>{full_name}</div>
+                        <div className="nameLabel">{full_name}</div>
                         <div>{email}</div>
                         <div>{phone}</div>
                         <div>{address}</div>
