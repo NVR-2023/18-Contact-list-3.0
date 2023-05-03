7760b8b1371f748f20a07880a58adaf04d1e4ea3
NVR-2023 <123973723+NVR-2023@users.noreply.github.com>
2023-05-03 13:04:46 +0000

    UI 03/05/2023

--- src/js/component/contactCard.js
+++ src/js/component/contactCard.js
@@ -32,13 +32,13 @@ const ContactCard = ({ index, avatarImage, fullName, email, phone, address }) =>
             <div className="card contactCard">
                 <div className="card-header cardHeader">
                     <div className="iconContainer">
-                        <span className="editIcon">
+                        <span className="editIcon clickable">
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-file-x" viewBox="0 0 16 16">
                                 <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
                                 <path d="M5 5h6v1H5V5zm0 3h6v1H5V8zm0 3h6v1H5v-1z" />
                             </svg>
                         </span>
-                        <span className="deleteIcon">
+                        <span className="deleteIcon clickable">
                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="bi bi-file-x" viewBox="0 0 16 16">
                                 <path d="M6.146 6.146a.5.5 0 0 1 .708 0L8 7.293l1.146-1.147a.5.5 0 1 1 .708.708L8.707 8l1.147 1.146a.5.5 0 0 1-.708.708L8 8.707 6.854 9.854a.5.5 0 0 1-.708-.708L7.293 8 6.146 6.854a.5.5 0 0 1 0-.708z" />
                                 <path d="M4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H4zm0 1h8a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1z" />
@@ -46,7 +46,7 @@ const ContactCard = ({ index, avatarImage, fullName, email, phone, address }) =>
                         </span>
                     </div>
                     <div className="avatarOuterContainer">
-                        <div className="avatarInnerContainer" style={{ backgroundColor: avatarCircleColor }}>
+                        <div className="avatarInnerContainer clickable" style={{ backgroundColor: avatarCircleColor }}>
                             {avatarImage ? (
                                 <img src={avatarImage} alt="avatar" />
                             ) : (
@@ -64,10 +64,10 @@ const ContactCard = ({ index, avatarImage, fullName, email, phone, address }) =>
                         <label>address:</label>
                     </div>
                     <div className="cardInfo">
-                        <div className="nameLabel">{fullName}</div>
-                        <div>{email}</div>
-                        <div>{phone}</div>
-                        <div>{address}</div>
+                        <div className="nameLabel clickable">{fullName}</div>
+                        <div className="clickable">{email}</div>
+                        <div className="clickable">{phone}</div>
+                        <div className="clickable">{address}</div>
                     </div>
                 </div>
             </div>
---
+++ src/js/views/createOrEditContact.js
@@ -0,0 +1,25 @@
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const CreateOrEditContact = () => {
+    return (
+    <>
+      <div>
+        CreateOrEditConatct
+      </div>
+      <div>
+        CreateOrEditConatct
+      </div>
+      <div>
+        CreateOrEditConatct
+      </div>
+      <div>
+        CreateOrEditConatct
+      </div>
+      <div>
+        CreateOrEditConatct
+      </div>
+    </>
+    );
+};
